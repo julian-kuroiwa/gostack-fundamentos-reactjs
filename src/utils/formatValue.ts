1ce50987e08cd1be04577320d9f04d84d6cdae06
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-04-23 17:50:59 -0300

    listing total balance inside the cards and all the transactions

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(value);
 
 export default formatValue;
